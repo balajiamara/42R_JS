03039dfd65bbba609bb1de40e5da306e2d5e3611
Balaji Amara <balajiamara2004@gmail.com>
2025-08-18 23:44:50 +0530

    Todo list

---
+++ ToDoList/script.js
@@ -0,0 +1,28 @@
+const inputbox=document.getElementById('inputbox')
+const listContainer=document.getElementById('list')
+
+function addTask(){
+    if (inputbox.value==''){
+        alert('Please enter an item')
+    }
+    else{
+        let li=document.createElement('li');
+        li.innerHTML=inputbox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.classList.add('closebut');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputbox.value = '';
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+}, false);
