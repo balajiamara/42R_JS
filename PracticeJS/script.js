90b550ff2955fcb7d702493b1a416886702979c9
Balaji Amara <balajiamara2004@gmail.com>
2025-08-29 23:00:36 +0530

    practice

---
+++ PracticeJS/script.js
@@ -0,0 +1,27 @@
+// let input = prompt('enter your value:')
+// console.log(input)
+//   let num = Number(input);   // convert to Number
+//   let bool = Boolean(input); // convert to Boolean
+//   let str = String(input);   // explicitly String
+
+//   console.log("Original:", input, "→", typeof input);
+//   console.log("Number:", num, "→", typeof num);
+//   console.log("Boolean:", bool, "→", typeof bool);
+//   console.log("String:", str, "→", typeof str);
+
+
+// let res=Number(input)
+// console.log(res+50)
+// console.log('5' + 2);
+// console.log("5" - 2);
+// console.log("5" * "2");
+// console.log("10" / true);
+
+// function toBoolean(value) {
+//   return !!value;  // double NOT converts to true/false
+// }
+
+// console.log(toBoolean(0));        // false
+// console.log(toBoolean(1)); 
+
+console.log(Boolean('0'))
---
+++ PracticeJS/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let input = prompt('enter your value:')
+// console.log(input)
+//   let num = Number(input);   // convert to Number
+//   let bool = Boolean(input); // convert to Boolean
+//   let str = String(input);   // explicitly String
+
+//   console.log("Original:", input, "→", typeof input);
+//   console.log("Number:", num, "→", typeof num);
+//   console.log("Boolean:", bool, "→", typeof bool);
+//   console.log("String:", str, "→", typeof str);
