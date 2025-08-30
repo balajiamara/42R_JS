9dd3e38a7c09059d8ee8051751700a0ce0419b54
Balaji Amara <balajiamara2004@gmail.com>
2025-08-30 20:32:14 +0530

    post updated

--- APIs/script.js
+++ APIs/script.js
@@ -1,7 +1,34 @@
-fetch("https://fakestoreapi.in/api/products")
-.then(function(response){
-    return response.json()
-})
-  .then(function(data) {
-    console.log(data);        // Handle the JSON data
-  })
+// HOW TO GET DATA FROM API
+// fetch("https://gorest.co.in/public/v2/users ")
+// .then(function(response){
+//     return response.json()
+// })
+//   .then(function(data) {
+//     console.log(data);        // Handle the JSON data
+//   })
+
+
+// HOW TO POST DATA TO API 
+
+// let newUser_1={
+//   name:'arjun',
+//   email:'arjun123@gmail.com',
+//   gender:'male',
+//   status:'active'
+// }
+
+// fetch('https://gorest.co.in/public/v2/users ',{
+//   method:'POST',
+//   body:JSON.stringify(newUser_1),
+//   headers:{
+//     'Content-type':'application/json',
+//     Accept:'application/json',
+//     Authorization:'Bearer b5c0eb640e976d0c854a4425023a7f70cbffa9b53e8baa63855d2888f12aa535'
+//   }
+// }).then(function(response){
+//   return response.json()
+// })
+// .then(function(jsondata){
+//   console.log(jsondata)
+// })
+
