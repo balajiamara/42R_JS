cb25a92d86d8e8a4a90667cba3c02fd8e17520a0
Balaji Amara <balajiamara2004@gmail.com>
2025-08-30 21:12:44 +0530

    DELETION UPDATED

--- APIs/script.js
+++ APIs/script.js
@@ -32,3 +32,46 @@
 //   console.log(jsondata)
 // })
 
+
+
+//how to update an API
+
+// let user2={
+//   name:'Ayeshaa',
+// }
+
+// fetch('https://gorest.co.in/public/v2/users/8096911',{
+//   method:'PUT',       //it is used to update one field
+//   body:JSON.stringify(user2),
+//   headers:{
+//     'Content-Type':'application/json',
+//     Accept:'application/json',
+//     Authorization:'Bearer b5c0eb640e976d0c854a4425023a7f70cbffa9b53e8baa63855d2888f12aa535'
+//   }
+// }).then(function(res){
+//   return res.json()
+// }).then(function(updated_data){
+//   console.log(updated_data)
+// })
+
+
+
+
+
+
+
+// HOW TO DELETE DATA FROM API
+// FOR DELETION NO NEED TO WRITE BODY, CONTENT-TYPE BECAUSE WS DON'T HAVE TO PROVIDE ANY USER DATA
+
+// fetch('https://gorest.co.in/public/v2/users/8096913',{
+//   method:'DELETE',
+//   headers:{
+//     Accept:'application/json',
+//     Authorization:'Bearer b5c0eb640e976d0c854a4425023a7f70cbffa9b53e8baa63855d2888f12aa535'
+//   }
+// }).then(function(del){
+//   return del.json()
+// }).then(function(del_data){
+//   console.log(del_data)
+// })
+
