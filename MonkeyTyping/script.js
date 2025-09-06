5503b1329a2cc29d0c84696ff77546cd5dc70419
Balaji Amara <balajiamara2004@gmail.com>
2025-09-06 22:17:28 +0530

    monkey typing

--- MonkeyTyping/script.js
+++ MonkeyTyping/script.js
@@ -1,7 +1,16 @@
 let user_input=document.getElementById('input')
-
+let sentence_ele=document.getElementById('sentence')
+let value=sentence_ele.textContent
+sentence_ele.innerHTML=value.split('').map(e=>`<span>${e}</span>`).join('')
 user_input.addEventListener('keyup',function(e){
-    for(let i=0; i<user_input.ariaValueMax.length;i++){
-        console.log(user_input.value[i])
+    let chr_elements=document.querySelectorAll('span')
+
+    for(let i=0; i<user_input.value.length;i++){
+        if(user_input.value[i]==chr_elements[i].textContent){
+            console.log(user_input.value[i],value[i],'hello')
+            chr_elements[i].style.color='green'
+        }else{
+            chr_elements[i].style.color='red'
+        }
     }
 })
